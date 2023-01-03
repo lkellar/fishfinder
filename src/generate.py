41fc143ebeec7b2dab29adc1f2433ba2b7d74603
Lucas K <lucas@lkellar.org>
2023-01-03 12:56:55 -0600

    more fixes

--- src/generate.py
+++ src/generate.py
@@ -1,7 +1,8 @@
-from src.fetch import fetch_all_fish_instances
+from src.fetch import fetch_all_fish_instances, EASTERN
 import dominate
 from dominate.tags import * 
 from datetime import datetime
+import pytz
 
 def generate_page(fish_data):
     doc = dominate.document(title='Fish Finder - University of Michigan')
@@ -12,9 +13,12 @@ def generate_page(fish_data):
         meta(name="description", content="The fastest way to locate dining hall fish at the University of Michigan")
         link(rel="stylesheet", href="style.css")
         
+    now = datetime.now(tz=EASTERN).strftime('%A, %B %d, %Y %H:%M:%S')
     with doc.body:
         with main():
             h1("Fish Finder")
+            p(f'Last Updated at {now}', id="last_updated")
+            hr()
             for index, day in enumerate(fish_data):
                 with div(_class="day"):
                     h2(day['date'].strftime('%A, %B %d, %Y'))
@@ -25,12 +29,14 @@ def generate_page(fish_data):
                                 for item in course["items"]:
                                     li(item)
                             
-                if index < len(fish_data) - 1:
                     hr()
+                    
+            with a(href="https://github.com/lkellar/fishfinder"):
+                p('github.com/lkellar/fishfinder')
                 
     with open('out.html', 'w') as f:
         f.write(doc.render())
 
-EXAMPLE_FISH_DATA = [{'date': datetime(2023, 1, 4, 13, 41, 19, 149573), 'data': {'Twigs at Oxford': [{'course': 'Lunch', 'items': ['Whitefish w/ Mango Pineapple Salsa']}]}, 'days_until': 1}, {'date': datetime(2023, 1, 11, 13, 42, 39, 161323), 'data': {'South Quad': [{'course': 'Lunch', 'items': ['MSC Fish Sandwich on White Bun', 'Baked Rockfish']}, {'course': 'Dinner', 'items': ['Crispy Rockfish']}]}, 'days_until': 8}, {'date': datetime(2023, 1, 13, 13, 43, 4, 103163), 'data': {'South Quad': [{'course': 'Dinner', 'items': ['Smothered Catfish']}]}, 'days_until': 10}, {'date': datetime(2023, 1, 16, 13, 43, 44, 317924), 'data': {'South Quad': [{'course': 'Dinner', 'items': ['MSC Grilled Cod Fish Taco']}], 'Bursley': [{'course': 'Dinner', 'items': ['MSC Grilled Cod Fish Taco']}], 'Twigs at Oxford': [{'course': 'Dinner', 'items': ['MSC Grilled Cod Fish Taco']}]}, 'days_until': 13}]
+#EXAMPLE_FISH_DATA = [{'date': datetime(2023, 1, 4, 13, 41, 19, 149573), 'data': {'Twigs at Oxford': [{'course': 'Lunch', 'items': ['Whitefish w/ Mango Pineapple Salsa']}]}, 'days_until': 1}, {'date': datetime(2023, 1, 11, 13, 42, 39, 161323), 'data': {'South Quad': [{'course': 'Lunch', 'items': ['MSC Fish Sandwich on White Bun', 'Baked Rockfish']}, {'course': 'Dinner', 'items': ['Crispy Rockfish']}]}, 'days_until': 8}, {'date': datetime(2023, 1, 13, 13, 43, 4, 103163), 'data': {'South Quad': [{'course': 'Dinner', 'items': ['Smothered Catfish']}]}, 'days_until': 10}, {'date': datetime(2023, 1, 16, 13, 43, 44, 317924), 'data': {'South Quad': [{'course': 'Dinner', 'items': ['MSC Grilled Cod Fish Taco']}], 'Bursley': [{'course': 'Dinner', 'items': ['MSC Grilled Cod Fish Taco']}], 'Twigs at Oxford': [{'course': 'Dinner', 'items': ['MSC Grilled Cod Fish Taco']}]}, 'days_until': 13}]
 
-generate_page(EXAMPLE_FISH_DATA)
+generate_page(fetch_all_fish_instances())
